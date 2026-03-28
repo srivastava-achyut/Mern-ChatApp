9463cea94c17474ceca9caf13dc2bbbaec8a51e0
Achyut Srivastava <achyutnishant08@gmail.com>
2026-03-28 19:32:27 +0530

    Update App.jsx to use a responsive container

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap'; // Assuming you're using react-bootstrap
+
+function App() {
+    return (
+        <Container fluid className="p-3">
+            {/* Your components go here */}
+            <h1>Welcome to the Mern Chat App</h1>
+        </Container>
+    );
+}
+
+export default App;
